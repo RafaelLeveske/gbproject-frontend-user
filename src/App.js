1075f1def97551098510caa50e02dabe9def06ec
Rafael Vieira <61164438+RafaelLeveske@users.noreply.github.com>
2020-07-18 18:59:52 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+// import './App.css';
+import api from './services/api';
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setProducts(response.data);
+      console.log(response.data);
+    })
+  }, []);
+
+
+  return (
+    <div>
+      {products.map(product => {
+        return (
+        <div key={product.id}>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+        </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
